4d72ef2e8ba748fbe0e03781c16a15fc428c619f
7m7s7 <staniszewski.michal94@gmail.com>
2025-03-02 09:52:24 +0100

    Initial commit

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return('<h1> hello cicd </h1>')
+
+if __name__ == '__main__':
+    application.run(debug=True)
+
