329bb6b3f7a0161dfa47abd96410a46f8e2ef951
7m7s7 <staniszewski.michal94@gmail.com>
2025-03-02 12:23:05 +0100

    actions

--- application.py
+++ application.py
@@ -7,8 +7,11 @@ ENV = os.environ.get('FLASK_ENV', 'dev')
 
 @application.route('/')
 def hello_world():
-    return('<h1> hello cicd </h1>'
-           f'<p>Środowisko: {ENV} </p>')
+    if ENV == 'dev':
+        return 'tadam'
+    else:
+        return('<h1> hello cicd </h1>'
+               f'<p>Środowisko: {ENV} </p>')
 
 if __name__ == '__main__':
     application.run(debug=True)
