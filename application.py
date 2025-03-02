e20cee6b0fb5eb190f7b3eac917feefe4d7fa684
7m7s7 <staniszewski.michal94@gmail.com>
2025-03-02 12:12:41 +0100

    actions

--- application.py
+++ application.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
 
 application = Flask(__name__)
 
+ENV = os.environ.get('FLASK_ENV', 'dev')
+
 @application.route('/')
 def hello_world():
-    return('<h1> hello cicd </h1>')
+    return('<h1> hello cicd </h1>'
+           f'<p>Środowisko: {ENV} </p>')
 
 if __name__ == '__main__':
     application.run(debug=True)
