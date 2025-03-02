2d5b0e29a0fd9169070d59cb90417fa703155513
7m7s7 <staniszewski.michal94@gmail.com>
2025-03-02 11:10:52 +0100

    actions

---
+++ test_application.py
@@ -0,0 +1,24 @@
+import unittest
+from application import application
+
+class FlaskAppTest(unittest.TestCase):
+    def setUp(self):
+        self.app= application.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        date = response.data.decode('utf-8')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('hello big data z pythonem', date)
+
+    def test_status_hello_page(self):
+        response=self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
